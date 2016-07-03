2a2329726e1420190feae92cd2bc2695c90b3dd0
ktsn <ktsn55@gmail.com>
2016-07-04 01:10:44 +0900

    Rename index.ts -> ob.ts

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
-  entry: './index.ts',
+  entry: './ob.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'ob.js',
